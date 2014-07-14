9b46a884d28a131db650176ff89e38c9212fb104
nagpal-saurav <saurav.nagpal@administrators-Mac-mini-6.local>
2014-07-14 17:08:42 +0530

    Adding the Face gesture sample to start work.

---
+++ FaceGestureSample/FaceGestureSample/Classes/Controller/UIViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceGestureSample
+//
+//  Created by Saurav Nagpal on 14/07/14.
+//  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
