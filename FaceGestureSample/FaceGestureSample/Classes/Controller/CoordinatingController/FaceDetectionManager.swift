7e4fb3bb9e75722cd994b1ab9d02fa1221372b02
nagpal-saurav <saurav.nagpal@administrators-Mac-mini-6.local>
2014-07-14 17:39:19 +0530

    Add the core image framework.

---
+++ FaceGestureSample/FaceGestureSample/Classes/Controller/CoordinatingController/FaceDetectionManager.swift
@@ -0,0 +1,15 @@
+//
+//  FaceDetectionManager.swift
+//  FaceGestureSample
+//
+//  Created by Saurav Nagpal on 14/07/14.
+//  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
+//
+
+
+import Foundation
+import CoreImage;
+
+class FaceDetectionManager{
+    
+}
