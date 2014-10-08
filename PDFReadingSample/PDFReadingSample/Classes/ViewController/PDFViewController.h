8df781f5df4f8a01266ca9db7669c3c0792d1dfd
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-09 00:15:12 +0530

    First Checkin for the PDF Viewer

---
+++ PDFReadingSample/PDFReadingSample/Classes/ViewController/PDFViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFReadingSample
+//
+//  Created by Saurav Nagpal on 08/10/14.
+//  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFViewController : UIViewController
+
+
+@end
+
---
+++ PDFReadingSample/PDFReadingSample/Classes/ViewController/PDFViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PDFReadingSample
+//
+//  Created by Saurav Nagpal on 08/10/14.
+//  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
+//
+
+#import "PDFViewController.h"
+
+@interface PDFViewController ()
+
+@end
+
+@implementation PDFViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
