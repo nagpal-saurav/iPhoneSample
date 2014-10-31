15087fa5a984f6ec20f5d7c7865aa581852c13a8
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-31 16:30:36 +0530

    Implement the SNSlide menu

---
+++ SNSideMenu/SNSideMenu/Classes/AppBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNSideMenu
+//
+//  Created by Saurav Nagpal on 31/10/14.
+//  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define URL     @"http://code.tutsplus.com/tutorials/implementing-container-containment-sliding-menu-controller--mobile-14562"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
